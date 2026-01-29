cc60e1f9c7343be61fee6cf19eaa727457550824
wooomr2 <wooomr2@gmail.com>
2026-01-29 10:46:15 +0900

    spring-ai advisors chat-memory prac

--- src/main/java/com/woo/ai/config/ChatClientConfig.java
+++ src/main/java/com/woo/ai/config/ChatClientConfig.java
@@ -1,6 +1,9 @@
 package com.woo.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,16 @@ public class ChatClientConfig {
 
     @Bean
     ChatClient chatClient(OpenAiChatModel openAiChatModel) {
-        return ChatClient.builder(openAiChatModel).build();
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .maxMessages(20)
+                .build();
+
+        MessageChatMemoryAdvisor chatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory)
+                .build();
+
+        return ChatClient.builder(openAiChatModel)
+                .defaultAdvisors(chatMemoryAdvisor)
+                .build();
     }
 }
---
+++ src/main/java/com/woo/ai/controller/ChatController.java
@@ -0,0 +1,29 @@
+package com.woo.ai.controller;
+
+import com.woo.ai.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @GetMapping("/chat")
+    public String memory(@RequestParam String id, @RequestParam String q) {
+        return chatService.chatMemory(q, id);
+    }
+
+    @GetMapping(value = "/chat/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> memoryStream(@RequestParam("id") String id,
+                                                      @RequestParam("q") String q) {
+        return chatService.chatMemoryStream(q, id)
+                .map(chunk -> ServerSentEvent.builder(chunk).build());
+    }
+}
